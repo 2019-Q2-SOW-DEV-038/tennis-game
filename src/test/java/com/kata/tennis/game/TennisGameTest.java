943632e0e5630aaa58b553a68c0f95041c49eeab
Viswanath Mohan <vishwa.mdu@gmail.com>
2019-08-01 19:44:00 +0200

    Refactor: Use constants fields in test cases instead of string literals

--- src/test/java/com/kata/tennis/game/TennisGameTest.java
+++ src/test/java/com/kata/tennis/game/TennisGameTest.java
@@ -4,26 +4,31 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class TennisGameTest {
-
+	
+	private static final String PLAYER_ONE_NAME="Federer";
+	private static final String PLAYER_TWO_NAME="Nadal";
+	
+	private static final int POINT_ONE=1;
+	
 	@Test
 	public void shouldCreateNewTennisGameWithTwoPlayers() {
-		TennisGame tennisGame = new TennisGame("Federer", "Nadal");
-		Assert.assertEquals("Federer", tennisGame.getPlayerOneName());
-		Assert.assertEquals("Nadal", tennisGame.getPlayerTwoName());
+		TennisGame tennisGame = new TennisGame(PLAYER_ONE_NAME, PLAYER_TWO_NAME);
+		Assert.assertEquals(PLAYER_ONE_NAME, tennisGame.getPlayerOneName());
+		Assert.assertEquals(PLAYER_TWO_NAME, tennisGame.getPlayerTwoName());
 	}
 	
 	@Test
 	public void playerOneScoreIncreasesAfterWinningPoint() {
-		TennisGame tennisGame = new TennisGame("Federer", "Nadal");
+		TennisGame tennisGame = new TennisGame(PLAYER_ONE_NAME, PLAYER_TWO_NAME);
 		tennisGame.playerOneScoresPoint();
-		Assert.assertEquals(1, tennisGame.getPlayerOnePoint());
+		Assert.assertEquals(POINT_ONE, tennisGame.getPlayerOnePoint());
 	}
 	
 	@Test
 	public void playerTwoScoreIncreasesAfterWinningPoint() {
-		TennisGame tennisGame = new TennisGame("Federer", "Nadal");
+		TennisGame tennisGame = new TennisGame(PLAYER_ONE_NAME, PLAYER_TWO_NAME);
 		tennisGame.playerTwoScoresPoint();
-		Assert.assertEquals(1, tennisGame.getPlayerTwoPoint());
+		Assert.assertEquals(POINT_ONE, tennisGame.getPlayerTwoPoint());
 	}
 
 }
