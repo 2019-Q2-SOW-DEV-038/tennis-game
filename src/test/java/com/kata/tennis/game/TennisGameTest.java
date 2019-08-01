fbd1d2506fcf48fed0a38276cf7f63fd72ad3d4a
Viswanath Mohan <vishwa.mdu@gmail.com>
2019-08-01 20:50:22 +0200

    Refactor: Initialize new game for each test

--- src/test/java/com/kata/tennis/game/TennisGameTest.java
+++ src/test/java/com/kata/tennis/game/TennisGameTest.java
@@ -1,6 +1,7 @@
 package com.kata.tennis.game;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import com.kata.tennis.model.Player;
@@ -12,36 +13,39 @@ public class TennisGameTest {
 	
 	private static final int POINT_ONE=1;
 	
+	TennisGame tennisGame;
+	
+	@Before
+	public void setup()
+	{
+		tennisGame = new TennisGame(new Player(PLAYER_ONE_NAME), new Player(PLAYER_TWO_NAME));
+	}
+	
 	@Test
 	public void shouldCreateNewTennisGameWithTwoPlayers() {
-		TennisGame tennisGame = new TennisGame(new Player(PLAYER_ONE_NAME), new Player(PLAYER_TWO_NAME));
 		Assert.assertEquals(PLAYER_ONE_NAME, tennisGame.getPlayerOne().getName());
 		Assert.assertEquals(PLAYER_TWO_NAME, tennisGame.getPlayerTwo().getName());
 	}
 	
 	@Test
 	public void playerOneScoreIncreasesAfterWinningPoint() {
-		TennisGame tennisGame = new TennisGame(new Player(PLAYER_ONE_NAME), new Player(PLAYER_TWO_NAME));
 		tennisGame.scorePoint(PLAYER_ONE_NAME);
 		Assert.assertEquals(POINT_ONE, tennisGame.getPlayerOne().getPoint());
 	}
 	
 	@Test
 	public void playerTwoScoreIncreasesAfterWinningPoint() {
-		TennisGame tennisGame = new TennisGame(new Player(PLAYER_ONE_NAME), new Player(PLAYER_TWO_NAME));
 		tennisGame.scorePoint(PLAYER_TWO_NAME);
 		Assert.assertEquals(POINT_ONE, tennisGame.getPlayerTwo().getPoint());
 	}
 	
 	@Test
 	public void shouldReturnLoveAllWhenScoreIsLoveAll() {
-		TennisGame tennisGame = new TennisGame(new Player(PLAYER_ONE_NAME), new Player(PLAYER_TWO_NAME));
 		Assert.assertEquals("Love-All", tennisGame.determineScore());
 	}
 	
 	@Test
 	public void shouldReturnFifteenLoveWhenScoreIsFifteenLove() {
-		TennisGame tennisGame = new TennisGame(new Player(PLAYER_ONE_NAME), new Player(PLAYER_TWO_NAME));
 		tennisGame.scorePoint(PLAYER_ONE_NAME);
 		Assert.assertEquals("Fifteen-Love", tennisGame.determineScore());
 	}
