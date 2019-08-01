3b63a44d7db431d938b247169160dd43aea9e808
Viswanath Mohan <vishwa.mdu@gmail.com>
2019-08-01 22:21:04 +0200

    Refactor: Used test case with parameters instead of duplicating tests

--- src/test/java/com/kata/tennis/game/TennisGameTest.java
+++ src/test/java/com/kata/tennis/game/TennisGameTest.java
@@ -3,36 +3,40 @@ package com.kata.tennis.game;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import com.kata.tennis.model.Player;
 
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
+
+@RunWith(JUnitParamsRunner.class)
 public class TennisGameTest {
-	
-	private static final String PLAYER_ONE_NAME="Federer";
-	private static final String PLAYER_TWO_NAME="Nadal";
-	
-	private static final int POINT_ONE=1;
-	
+
+	private static final String PLAYER_ONE_NAME = "Federer";
+	private static final String PLAYER_TWO_NAME = "Nadal";
+
+	private static final int POINT_ONE = 1;
+
 	TennisGame tennisGame;
-	
+
 	@Before
-	public void setup()
-	{
+	public void setup() {
 		tennisGame = new TennisGame(new Player(PLAYER_ONE_NAME), new Player(PLAYER_TWO_NAME));
 	}
-	
+
 	@Test
 	public void shouldCreateNewTennisGameWithTwoPlayers() {
 		Assert.assertEquals(PLAYER_ONE_NAME, tennisGame.getPlayerOne().getName());
 		Assert.assertEquals(PLAYER_TWO_NAME, tennisGame.getPlayerTwo().getName());
 	}
-	
+
 	@Test
 	public void playerOneScoreIncreasesAfterWinningPoint() {
 		tennisGame.scorePoint(PLAYER_ONE_NAME);
 		Assert.assertEquals(POINT_ONE, tennisGame.getPlayerOne().getPoint());
 	}
-	
+
 	@Test
 	public void playerTwoScoreIncreasesAfterWinningPoint() {
 		tennisGame.scorePoint(PLAYER_TWO_NAME);
@@ -40,36 +44,22 @@ public class TennisGameTest {
 	}
 	
 	@Test
-	public void shouldReturnLoveAllWhenScoreIsLoveAll() {
-		Assert.assertEquals("Love-All", tennisGame.determineScore());
+    @Parameters({
+    	"0, 0, Love-All",
+        "1, 0, Fifteen-Love", 
+        "0, 1, Love-Fifteen", 
+        "1, 1, Fifteen-All" })
+	public void shouldReturnScoreInTennisFormatBasedOnThePointsScoredByPlayers(int playerOnePoint, int playerTwoPoint,
+			String gameScore) {
+		createScore(playerOnePoint, playerTwoPoint);
+		Assert.assertEquals(gameScore, tennisGame.determineScore());
 	}
-	
-	@Test
-	public void shouldReturnFifteenLoveWhenScoreIsFifteenLove() {
-		createScore(1,0);
-		Assert.assertEquals("Fifteen-Love", tennisGame.determineScore());
-	}
-	
-	@Test
-	public void shouldReturnLoveFifteenWhenScoreIsLoveFifteen() {
-		createScore(0,1);
-		Assert.assertEquals("Love-Fifteen", tennisGame.determineScore());
-	}
-	
-	@Test
-	public void shouldReturnFifteenAllWhenScoreIsFifteenAll() {
-		createScore(1,1);
-		Assert.assertEquals("Fifteen-All", tennisGame.determineScore());
-	}
-	
-	private void createScore(int playerOnePoint,int playerTwoPoint)
-	{
-		for(int pointCounter=0;pointCounter<playerOnePoint;pointCounter++)
-		{
+
+	private void createScore(int playerOnePoint, int playerTwoPoint) {
+		for (int pointCounter = 0; pointCounter < playerOnePoint; pointCounter++) {
 			tennisGame.scorePoint(PLAYER_ONE_NAME);
 		}
-		for(int pointCounter=0;pointCounter<playerTwoPoint;pointCounter++)
-		{
+		for (int pointCounter = 0; pointCounter < playerTwoPoint; pointCounter++) {
 			tennisGame.scorePoint(PLAYER_TWO_NAME);
 		}
 	}
