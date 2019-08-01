ef303046b356738941f8a26f31489482a6e4fbfd
Viswanath Mohan <vishwa.mdu@gmail.com>
2019-08-01 19:19:11 +0200

    Create new tennis game with two players

---
+++ src/main/java/com/kata/tennis/game/TennisGame.java
@@ -0,0 +1,14 @@
+package com.kata.tennis.game;
+
+public class TennisGame {
+	
+	public String playerOneName;
+	public String playerTwoName;
+	
+	public TennisGame(String playerOneName, String playerTwoName) {
+		this.playerOneName = playerOneName;
+		this.playerTwoName = playerTwoName;
+	}
+	
+	
+}
---
+++ src/test/java/com/kata/tennis/game/TennisGameTest.java
@@ -0,0 +1,15 @@
+package com.kata.tennis.game;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TennisGameTest {
+
+	@Test
+	public void shouldCreateNewTennisGameWithTwoPlayers() {
+		TennisGame tennisGame = new TennisGame("Federer", "Nadal");
+		Assert.assertEquals("Federer", tennisGame.playerOneName);
+		Assert.assertEquals("Nadal", tennisGame.playerTwoName);
+	}
+
+}
